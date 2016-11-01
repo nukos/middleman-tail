042c89d51aa8b299b796d0d528d0c4b7c9dcc69e
nukos <nukos.kitchen@gmail.com>
2016-11-01 11:06:11 +0900

    add FontAwesome

--- README.md
+++ README.md
@@ -2,15 +2,7 @@ middleman-tail
 =====================================
 **middleman-tail** is the Skeleton to create a blog that uses a middleman 4.
 
-1, System dependencies
--------------------
-* [rbenv](https://github.com/sstephenson/rbenv) (Ruby: 2.3.1)
-* [ndenv](https://github.com/riywo/ndenv) (node.js: 6.6.0)
-* [bundler](http://bundler.io/) (1.13.5)
-* [middleman](https://middlemanapp.com/jp/) (4.1.10)
-* [gulp](https://github.com/gulpjs/gulp)
-
-### 1.1, Feature
+### Feature
 * Pre install and Settings
   * middleman-blog
   * middleman-syntax
@@ -19,9 +11,18 @@ middleman-tail
   * middleman-minify-html
 * External pipeline support (Blowserify and Sass using Gulp)
   * Blowserify
-  * Sass(Bourbon and Neat)
+  * Sass(Bourbon, Neat, Font-Awesome)
 * Slim Templates
 * Default timezone: Asia/Tokyo (UTC+9)
+
+
+1, System dependencies
+-------------------
+* [rbenv](https://github.com/sstephenson/rbenv) (Ruby: 2.3.1)
+* [ndenv](https://github.com/riywo/ndenv) (node.js: 6.6.0)
+* [bundler](http://bundler.io/) (1.13.5)
+* [middleman](https://middlemanapp.com/jp/) (4.1.10)
+* [gulp](https://github.com/gulpjs/gulp)
 
 2, Getting Started
 ------------
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-var gulp       = require('gulp');
-var browserify = require('browserify');
-var source     = require('vinyl-source-stream');
-var watchify   = require('watchify');
-var sass       = require('gulp-sass');
-var bourbon    = require('node-bourbon');
-var neat       = require('node-neat');
+var gulp        = require('gulp');
+var browserify  = require('browserify');
+var source      = require('vinyl-source-stream');
+var watchify    = require('watchify');
+var sass        = require('gulp-sass');
+var bourbon     = require('node-bourbon');
+var neat        = require('node-neat');
+var fontAwesome = require('node-font-awesome');
 
 // settings ---
 var jsConf = {
@@ -14,21 +15,26 @@ var jsConf = {
 }
 var cssConf = {
   srcPath:      'source/assets/stylesheets/**/*.scss',
-  destPath:     '.tmp/dest/assets/stylesheets'
+  destPath:     '.tmp/dest/assets/stylesheets',
+  includePaths: Array.prototype.concat( neat.includePaths, fontAwesome.scssPath )
+}
+var fontConf = {
+  destPath:     '.tmp/dest/assets/fonts'
 }
 
 var b = browserify({
-  entries: jsConf.srcPath,
-  cache: {},
+  entries:      jsConf.srcPath,
+  cache:        {},
   packageCache: {}
 });
 
 // gulp tasks ---
+gulp.task('bundle',  jsBundle);
+gulp.task('fonts',   copyFontAwesomeFonts);
+gulp.task('sass',    ['fonts'], sassPreCompile);
+gulp.task('build',   ['sass', 'bundle']);
+gulp.task('server',  ['sass', 'bundle'], watch);
 gulp.task('default', ['build']);
-gulp.task('build', ['sass', 'bundle']);
-gulp.task('server', ['sass', 'bundle'], watch);
-gulp.task('bundle', jsBundle);
-gulp.task('sass', sassPreCompile);
 
 // functions ---
 function watch() {
@@ -50,8 +56,12 @@ function jsBundle() {
 function sassPreCompile(){
   gulp.src(cssConf.srcPath)
     .pipe(sass({
-      includePaths: neat.includePaths
+      includePaths: cssConf.includePaths
     }))
     .pipe(gulp.dest(cssConf.destPath));
 }
 
+function copyFontAwesomeFonts(){
+  gulp.src(fontAwesome.fonts)
+    .pipe(gulp.dest(fontConf.destPath));
+}
